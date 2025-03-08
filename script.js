e72a596725d4ce5c406f8597863e1ab209a2eb6b
skatactor <soweidyj@gmail.com>
2025-03-07 20:19:10 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function searchDeals() {
     let query = document.getElementById('search-box').value;
     if (query) {
         let amazonLink = `https://www.amazon.com/s?k=${encodeURIComponent(query)}&tag=allthedisco04-20`;
-        let resultDiv = document.getElementById('results');
-        resultDiv.innerHTML = `<p>Find on Amazon: <a href="${amazonLink}" target="_blank">${query} deals</a></p>`;
+        window.location.href = amazonLink;
     }
 }
