6ec69aff7e1c4c42e0860e5ac745fa3c87a6d6da
skatactor <soweidyj@gmail.com>
2025-03-07 19:16:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function searchDeals() {
+    let query = document.getElementById('search-box').value;
+    if (query) {
+        let amazonLink = `https://www.amazon.com/s?k=${encodeURIComponent(query)}&tag=your-affiliate-tag-20`;
+        let resultDiv = document.getElementById('results');
+        resultDiv.innerHTML = `<p>Find on Amazon: <a href="${amazonLink}" target="_blank">${query} deals</a></p>`;
+    }
+}
