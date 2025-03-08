29f12590a92cf765ef2e245adcab387e8ae30805
skatactor <soweidyj@gmail.com>
2025-03-08 10:14:37 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleSubcategories(categoryId) {
         subcategoryList.style.display = "none";
     }
 }
+
+function searchDeals() {
+    let query = document.getElementById('search-box').value;
+    let discount = document.getElementById('discount').value;
+    let amazonBaseURL = `https://www.amazon.com/s?k=${encodeURIComponent(query)}`;
+
+    if (discount !== "all") {
+        amazonBaseURL += `&rh=p_8%3A${discount}`;
+    }
+
+    amazonBaseURL += `&tag=allthedisco04-20`;
+    window.open(amazonBaseURL, "_blank");
+}
