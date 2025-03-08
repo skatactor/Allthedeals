24b341f6bdf69370ead418ed6979b792e31492c6
skatactor <soweidyj@gmail.com>
2025-03-07 21:01:44 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function searchDeals() {
     let query = document.getElementById('search-box').value;
     let discount = document.getElementById('discount').value;
     let amazonBaseURL = `https://www.amazon.com/s?k=${encodeURIComponent(query)}`;
-    
+
     if (discount !== "all") {
         amazonBaseURL += `&rh=p_8%3A${discount}`;
     }
