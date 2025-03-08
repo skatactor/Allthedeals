59fda6cbe11a1f377af631a621d2dbc8dfa13a79
skatactor <soweidyj@gmail.com>
2025-03-07 20:08:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function searchDeals() {
     let query = document.getElementById('search-box').value;
     if (query) {
-        let amazonLink = `https://www.amazon.com/s?k=${encodeURIComponent(query)}&tag=your-affiliate-tag-20`;
+        let amazonLink = `https://www.amazon.com/s?k=${encodeURIComponent(query)}&tag=allthedisco04-20`;
         let resultDiv = document.getElementById('results');
         resultDiv.innerHTML = `<p>Find on Amazon: <a href="${amazonLink}" target="_blank">${query} deals</a></p>`;
     }
