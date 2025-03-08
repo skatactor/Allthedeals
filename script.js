107929fefcc2be3fb342da8a8cd5b48198f1e95f
skatactor <soweidyj@gmail.com>
2025-03-07 20:37:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function searchDeals() {
     let query = document.getElementById('search-box').value;
-    if (query) {
-        let amazonLink = `https://www.amazon.com/s?k=${encodeURIComponent(query)}&tag=allthedisco04-20`;
-        window.location.href = amazonLink;
+    let discount = document.getElementById('discount').value;
+    let amazonBaseURL = `https://www.amazon.com/s?k=${encodeURIComponent(query)}`;
+    
+    if (discount !== "all") {
+        amazonBaseURL += `&rh=p_8%3A${discount}`;
     }
+
+    amazonBaseURL += `&tag=allthedisco04-20`;
+    window.location.href = amazonBaseURL;
 }
